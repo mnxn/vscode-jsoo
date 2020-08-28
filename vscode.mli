219d0829a4ee82b861660d937ba1ea70833a7f3a
Max Lantas <25037249+mnxn@users.noreply.github.com>
2020-08-27 22:07:05 -0700

    EndOfLine, TextEdit, Uri, TextDocument

--- vscode.mli
+++ vscode.mli
@@ -1,4 +1,31 @@
+[@@@js.stop]
+
+type regexp = Js_of_ocaml.Regexp.regexp
+
+[@@@js.start]
+
+[@@@js.implem
+type regexp = Js_of_ocaml.Regexp.regexp
+
+let regexp_to_js : regexp -> Ojs.t = Obj.magic
+
+let regexp_of_js : Ojs.t -> regexp = Obj.magic]
+
 val get_version : unit -> string [@@js.get "vscode.version"]
+
+module Disposable : sig
+  class t :
+    Ojs.t
+    -> object
+         inherit Ojs.obj
+
+         method dispose : unit -> unit
+       end
+
+  val make : dispose:(unit -> unit) -> unit -> t [@@js.new "Disposable"]
+
+  val from : (t list[@js.variadic]) -> t [@@js.global "Disposable.from"]
+end [@scope "vscode"]
 
 module Command : sig
   class t :
@@ -123,4 +150,121 @@ module TextLine : sig
     -> unit
     -> t
     [@@js.builder]
+end [@scope "vscode"]
+
+module EndOfLine : sig
+  type t =
+    | CRLF [@js 2]
+    | LF [@js 1]
+  [@@js.enum]
+end [@scope "vscode"]
+
+module TextEdit : sig
+  class t :
+    Ojs.t
+    -> object
+         inherit Ojs.obj
+
+         method range : Range.t
+
+         method new_text : string
+
+         method new_eol : EndOfLine.t
+       end
+
+  val create : range:Range.t -> new_text:string -> unit -> t [@@js.builder]
+
+  val replace : range:Range.t -> new_text:string -> unit -> t
+    [@@js.global "TextEdit.replace"]
+
+  val insert : position:Position.t -> new_text:string -> unit -> t
+    [@@js.global "TextEdit.insert"]
+
+  val delete : Range.t -> t [@@js.global "TextEdit.delete"]
+
+  val set_end_of_line : EndOfLine.t -> t [@@js.global "TextEdit.setEndOfLine"]
+end [@scope "vscode"]
+
+module Uri : sig
+  class t :
+    Ojs.t
+    -> object
+         inherit Ojs.obj
+
+         method scheme : string
+
+         method authority : string
+
+         method path : string
+
+         method query : string
+
+         method fragment : string
+
+         method fs_path : string
+
+         method to_string : ?skip_encoding:bool -> unit -> string
+
+         method to_json : unit -> Ojs.t
+         (* method with_ :
+               ?scheme:string
+            -> ?authority:string
+            -> ?path:string
+            -> ?query:string
+            -> ?fragment:string
+            -> unit
+            -> t *)
+       end
+
+  val parse : string -> ?strict:bool -> unit -> t [@@js.global "Uri.parse"]
+
+  val file : string -> t [@@js.global "Uri.parse"]
+
+  val join_path : t -> (string list[@js.variadic]) -> t
+    [@@js.global "Uri.joinPath"]
+end [@scope "vscode"]
+
+module TextDocument : sig
+  class t :
+    Ojs.t
+    -> object
+         inherit Ojs.obj
+
+         method uri : Uri.t
+
+         method file_name : string
+
+         method is_untitled : bool
+
+         method language_id : string
+
+         method version : int
+
+         method is_dirty : bool
+
+         method is_closed : bool
+
+         method eol : EndOfLine.t
+
+         method line_count : int
+
+         (* method save : unit -> bool Promise.t *)
+         method line_at : int -> TextLine.t
+
+         method line_at_position : Position.t -> TextLine.t
+         [@@js.call "lineAt"]
+
+         method offset_at : Position.t -> int
+
+         method position_at : int -> Position.t
+
+         method get_text : ?range:Range.t -> unit -> string
+
+         method get_word_range_at_position :
+           Position.t -> ?regex:regexp -> unit -> Range.t option
+
+         method validate_range : Range.t -> Range.t
+
+         method validate_position : Position.t -> Position.t
+       end
 end [@scope "vscode"]
